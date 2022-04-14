9774b5684606bcdc386d17907eb5204ba2b405d5
constanzananjari <constanza.nanjari.colillanca@alumnos.sip.clm>
2022-04-14 11:36:14 -0400

    segundocambio

---
+++ Proyectos/js Nanjari Constanza.js
@@ -0,0 +1,27 @@
+//js Nanjari Constanza
+
+//Preguntas:
+
+
+//¿Qué indicará console.log cuando se llame a esta función?
+
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+} 
+
+//¿qué indicará console.log?
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+
+
+//¿qué indicaría console.log?//
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
