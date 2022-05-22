e12f9654dee679cd51d8f86b8373322d763ffc7e
constanzananjarie <constranza.nanjari.colillanca@alumnos.sip.cl>
2022-05-22 15:16:22 -0400

    primercambio

---
+++ proyecto coding dojo/JS/Nanjari_Nombre_JS_jugueteo.js
@@ -0,0 +1,18 @@
+var alturaniño("1.60");
+
+function ElNiñoPuedeSubirALaMontañaRusa(x){
+
+
+//x =altura(1.60)
+if(x < 1.60);
+{
+console.log("Sí");
+}
+  else{
+      console.log("No");
+  }
+  
+  
+    
+
+}
